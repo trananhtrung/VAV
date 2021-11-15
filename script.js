7e6ff05f2700bbcc365b9a664e067bdbdcfc1f12
trananhtrung <71745159+trananhtrung@users.noreply.github.com>
2021-11-15 18:48:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ app.factory('recognizeService', function($http) {
     return {
         recognize: function(imgLink) {
             // Link tới RestAPI đã viết ở phần 
-            var url = 'https://wt-huyhoang8a5-gmail-com-0.run.webtask.io/idol-recognize&#39;;
+            var url = 'https://wt-huyhoang8a5-gmail-com-0.run.webtask.io/idol-recognize&#39';
             return $http({
                 method: 'POST',
                 url,
